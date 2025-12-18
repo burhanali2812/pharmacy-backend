52037847ae9c0b0a6066bad37c50ba717ea96fc9
burhanali2812 <syedburhanali830@gmail.com>
2025-12-18 08:09:19 +0500

    medicne schema

--- models/Medicine.js
+++ models/Medicine.js
@@ -1,15 +1,19 @@
 import mongoose from 'mongoose';
 
 const medicineShema = new mongoose.Schema({
-    sID: Number,
-    name: String,
-    quantity: Number,
-    price: Number,
-    expire: String,
-    prescription: String,
-    actualPrice: Number,
-    profit: Number,
-    supplier: String
+    sID: {type: Number, required: true , unique: true},
+    name:{
+        type: String,
+        required: true,
+        unique: true,
+    } ,
+    quantity:{type: Number, required: true},
+    price: {type: Number, required: true},
+    expire: {type: Date, required: true},
+    prescription: {type: String, required: true},
+    actualPrice: {type: Number, required: true},
+    profit: {type: Number, required: true},     
+    supplier: {type: String, required: true}
 });
 
 export default mongoose.model("Medicine", medicineShema);
